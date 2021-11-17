1a411a136b46786223c2f1e3659083ec6787ebd8
USER <ASD>
2021-11-17 14:06:08 +0200

    asd

--- app/src/main/java/com/example/gymforyou/ExercisesListActivity.java
+++ app/src/main/java/com/example/gymforyou/ExercisesListActivity.java
@@ -12,58 +12,79 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+public class ExercisesListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    Button toExercises;
+    ListView l1;
+    MyListAdapter adapter1;
+    ArrayList<String> aTitle = new ArrayList<>(), aDescription = new ArrayList<>();
     SharedPref sharedPref;
     AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         sharedPref = new SharedPref(this);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_exercises_list);
 
+        aTitle.add("Excersie 1");
+        aTitle.add("Excersie 2");
+        aTitle.add("Excersie 3");
+        aTitle.add("Excersie 4");
 
+        aDescription.add("blablabla");
+        aDescription.add("blablabla");
+        aDescription.add("blablabla");
+        aDescription.add("blablabla");
 
-        toExercises = findViewById(R.id.toExercises);
+        adapter1 = new MyListAdapter(this, aTitle, aDescription);
+
+        l1 = findViewById(R.id.LTest);
+        l1.setAdapter(adapter1);
 
         builder = new AlertDialog.Builder(this);
 
-        toExercises.setOnClickListener(this);
+        l1.setOnItemClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-        if(view == toExercises)
-        {
-            Intent intent1 = new Intent(this, ExercisesListActivity.class);
-            startActivity(intent1);
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Intent intent1 = new Intent(this, ExerciseDetailsActivity.class);
+        switch (position) {
+            case 0:
+                intent1.putExtra("WE", "Exercise 1");
+                break;
+            case 1:
+                intent1.putExtra("WE", "Exercise 2");
+                break;
+            case 2:
+                intent1.putExtra("WE", "Exercise 3");
+                break;
+            case 3:
+                intent1.putExtra("WE", "Exercise 4");
+                break;
         }
+
+        startActivity(intent1);
+        finish();
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
 
-        for (int i = 0; i < menu.size(); i++) {
-            MenuItem item = menu.getItem(i);
+        for(int i=0;i<menu.size();i++)
+        {
+            MenuItem item= menu.getItem(i);
             item.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
         }
-
         MenuItem item;
-        item = menu.getItem(4);
-        item.setEnabled(false);
-        item.setVisible(false);
 
         if (sharedPref.GetUsername().equals("YouRGuest")) {
             item = menu.getItem(0);
@@ -95,19 +116,22 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
 
         if (id == R.id.action_login) {
-            Intent intent = new Intent(this, LoginPage.class);
+            Intent intent=new Intent(this,LoginPage.class);
             startActivityForResult(intent, 0);
             //Toast.makeText(this,"you selected login",Toast.LENGTH_LONG).show();
             return true;
-        } else if (id == R.id.action_register) {
+        }
+        else if (id == R.id.action_register) {
             Intent intent = new Intent(this, SignUp.class);
             startActivityForResult(intent, 0);
             return true;
-        } else if (id == R.id.action_exit) {
+        }
+        else if (id == R.id.action_exit){
             builder.setMessage("Do you want to logout?")
                     .setCancelable(false)
                     .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -130,6 +154,10 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
             AlertDialog alert = builder.create();
             alert.setTitle("Logout");
             alert.show();
+        }
+        else if (id == R.id.action_GoHome) {
+            finish();
+            return true;
         } else if (id == R.id.action_SearchGyms) {
             // Search for gyms nearby
             Uri gmmIntentUri = Uri.parse("geo:0,0?q=gym");
@@ -159,7 +187,6 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         startActivity(i);
         finish();
     }
-
     void restartapp() {
         Intent i = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(i);

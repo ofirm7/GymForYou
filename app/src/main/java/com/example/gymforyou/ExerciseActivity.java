8490f9a8d12dbed6587ab87d9b2a99761793fce7
User <mkjnih>
2021-12-08 14:11:49 +0200

    fgh

--- app/src/main/java/com/example/gymforyou/ExerciseActivity.java
+++ app/src/main/java/com/example/gymforyou/ExerciseActivity.java
@@ -1,10 +1,13 @@
 package com.example.gymforyou;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -12,12 +15,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 public class ExerciseActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,6 +44,8 @@ public class ExerciseActivity extends AppCompatActivity implements View.OnClickL
     EditText exerciseDetailsEt, urlEv;
     TextView noVideoTv;
     Dialog addDialog;
+
+    ProgressDialog progressDialog;
 
     static String url = null;
     VideoView exerciseVideoExample;
@@ -148,7 +166,9 @@ public class ExerciseActivity extends AppCompatActivity implements View.OnClickL
 
             restartapp();
         } else if (view == openAddVideo) {
-            OpenAddVideoDialog();
+            // Code for showing progressDialog while uploading
+            progressDialog = new ProgressDialog(ExerciseActivity.this);
+            chooseVideo();
         }
         else if(view == deleteVideo)
         {
@@ -181,31 +201,72 @@ public class ExerciseActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
-    public void OpenAddVideoDialog() {
-        addDialog = new Dialog(this);
-        addDialog.setContentView(R.layout.custom_dialog_add_video_url);
-        addDialog.setTitle("Add Video");
+    // choose a video from phone storage
+    private void chooseVideo() {
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 5);
+    }
 
-        addDialog.setCancelable(true);
+    Uri videouri;
 
-        urlEv = addDialog.findViewById(R.id.urlEv);
-        addVideo = addDialog.findViewById(R.id.addVideo);
+    // startActivityForResult is used to receive the result, which is the selected video.
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 5 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            videouri = data.getData();
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+            uploadvideo();
+        }
+    }
 
-        addVideo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(v == addVideo)
-                {
-                    DataModel.muscles.get(getIntent().getIntExtra("WESEC", 0))
-                            .getExercisesList().get(getIntent().getIntExtra("ELTOE", 0)).setUrl(urlEv.getText().toString());
-                    DataModel.muscleSave();
-                    addDialog.dismiss();
-                    restartapp();
+    private String getfiletype(Uri videouri) {
+        ContentResolver r = getContentResolver();
+        // get the file type ,in this case its mp4
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(r.getType(videouri));
+    }
+
+    private void uploadvideo() {
+        if (videouri != null) {
+            // save the selected video in Firebase storage
+            final StorageReference reference = FirebaseStorage.getInstance().getReference("Files/" + System.currentTimeMillis() + "." + getfiletype(videouri));
+            reference.putFile(videouri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!uriTask.isSuccessful()) ;
+                    // get the link of video
+                    String downloadUri = uriTask.getResult().toString();
+                    DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference("Video");
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("videolink", downloadUri);
+                    reference1.child("" + System.currentTimeMillis()).setValue(map);
+                    // Video uploaded successfully
+                    // Dismiss dialog
+                    progressDialog.dismiss();
+                    Toast.makeText(ExerciseActivity.this, "Video Uploaded!!", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-
-        addDialog.show();
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Error, Image not uploaded
+                    progressDialog.dismiss();
+                    Toast.makeText(ExerciseActivity.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                // Progress Listener for loading
+                // percentage on the dialog box
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    // show the progress bar
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                }
+            });
+        }
     }
 
     @Override
@@ -286,14 +347,6 @@ public class ExerciseActivity extends AppCompatActivity implements View.OnClickL
             return true;
         }
         return true;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Intent i = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(i);
-        finish();
     }
 
     boolean isCreatorOrAdmin()
